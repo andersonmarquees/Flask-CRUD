0572e5cdb72b3953113a531af63363a6cf302fae
anderson <anderson_fisico@yahoo.com.br>
2020-01-27 16:37:32 -0300

    add template lista de jogos

--- app.py
+++ app.py
@@ -1,5 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
-app.run()
+
+class ListaJogo:
+    def __init__(self, nome, categoria, console):
+        self.nome = nome
+        self.categoria = categoria
+        self.console = console
+
+jogo1 = ListaJogo('Mario Bros', 'Aventura', 'SNES')
+jogo2 = ListaJogo('World of Warcraft', 'RPG', 'PC')
+jogo3 = ListaJogo('Tetris', 'RPG', 'PC')
+
+lista_jogos = [
+    jogo1,
+    jogo2,
+    jogo3
+]
+
+@app.route('/')
+def index():
+    return render_template('lista.html', titulo='Lista de Jogos', lista_jogos=lista_jogos)
